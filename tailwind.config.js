b62a68b2a320fd2e5930f63c53e7b76c41280694
Kev <kevin@MacBookPro.lan>
2024-10-15 16:46:48 +0200

    still in progress - involved part

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,15 @@ module.exports = {
       fontFamily: {
         halflings: ["halflings"],
       },
+      fontSize: {
+        '1.2rem': '1.2rem',
+      },
       backgroundImage: {
         'heroBg': "url('../images/hero-image.jpg')",
       },
+      colors: {
+        'primaryColor': "#F74C3D"
+      }
     },
   },
   plugins: [],
